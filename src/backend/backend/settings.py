17bc0261943a9be94d82a71ed485cb9a4ad15e80
James Yip <waterdragenarchive@gmail.com>
2023-12-06 19:52:09 +0800

    Update server port and debug

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*1cuxpc&&@ib)bp0dxy+u(brcw5%thdwp8qv0_1j_o3t02&+!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["waterdragen.onrender.com"]
 
