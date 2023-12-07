081fad1c1a7b581f9536d6411c75942d5588bb55
James Yip <waterdragenarchive@gmail.com>
2023-12-08 06:33:50 +0800

    Update cmini url

--- src/dist/consts.js
+++ src/dist/consts.js
@@ -1,3 +1,3 @@
 const SITE_URL = "https://waterdragen.onrender.com";
-const CMINI_URL = "wss://cmini.onrender.com:8000/cmini/";
+const CMINI_URL = "wss://cmini.onrender.com/cmini/";
 export { SITE_URL, CMINI_URL };
