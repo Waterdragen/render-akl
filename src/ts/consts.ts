f0ba168593e7660d4781cfc0dd840e3b074e1d8c
James Yip <waterdragenarchive@gmail.com>
2023-12-08 05:31:29 +0800

    Update cmini port to 8000

--- src/ts/consts.ts
+++ src/ts/consts.ts
@@ -1,4 +1,4 @@
 const SITE_URL: string = "https://waterdragen.onrender.com";
-const CMINI_URL: string = "wss://cmini.onrender.com";
+const CMINI_URL: string = "wss://cmini.onrender.com:8000";
 
 export {SITE_URL, CMINI_URL};
